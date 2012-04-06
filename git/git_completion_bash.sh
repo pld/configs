f330c1be604b64b20cff143ec19dc8e6a635988d
pld <peter@helioid.com>
2012-04-06 06:42:20 -0400

    git configs

---
+++ git/git_completion_bash.sh
@@ -0,0 +1,10 @@
+if [ -f /usr/local/git/contrib/completion/git-completion.bash ]; then
+  . /usr/local/git/contrib/completion/git-completion.bash
+fi
+GIT_PS1_SHOWDIRTYSTATE=true
+ 
+if [ -f /opt/local/etc/bash_completion ]; then
+    . /opt/local/etc/bash_completion
+fi
+
+PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[34m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
